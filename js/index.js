2d2f98dad8c7865aebe4fd9d0e8748e537cc618c
Ari Tomas Bussi <abussy@gmail.com>
2022-07-05 21:23:12 +0200

    Iteration 1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
+console.log("I'm ready!");
+
 // Iteration 1: Names and Input
-//
+const hacker1 = 'Ari';
+const hacker2 = 'Tombola';
+
+console.log("The driver's name is " + hacker1);
+console.log("The navigator's name is " + hacker2);
+
 // Iteration 2: Conditionals
 
 
