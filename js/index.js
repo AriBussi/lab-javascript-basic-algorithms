56bcc7e052c27c24897d88f93ce80e3fd3d0bdf7
Ari Tomas Bussi <abussy@gmail.com>
2022-07-07 20:35:03 +0200

    iteration 3

--- js/index.js
+++ js/index.js
@@ -21,3 +21,31 @@ if (hacker1.length > hacker2.length) {
 console.log(message);
 
 // Iteration 3: Loops
+// 3.1
+let uppercased = '';
+
+for (let i = 0; i < hacker1.length ; i++) {
+    uppercased = uppercased + hacker1[i].toUpperCase() + " ";
+}
+
+console.log(uppercased);
+
+//3.2
+let reversed = '';
+
+for (let i = hacker1.length - 1; i >= 0 ; i--) {
+    reversed = reversed + hacker1[i];
+}
+
+console.log(reversed);
+
+//3.3
+let lastLog = 'What?! You both have the same name?';
+
+if( hacker1 > hacker2) {
+    lastLog = "The driver's name goes first.";
+} else if ( hacker2 > hacker1) {
+    lastLog = "Yo, the navigator goes first definitely.";
+}
+
+console.log(lastLog);
